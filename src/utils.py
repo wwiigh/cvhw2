e31598a550e56ada24a719d7b3428e1620934a14
wwiigh <wwiigh@gmail.com>
2025-04-12 20:53:27 +0800

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,4 @@
 from torchvision import transforms
-import random
-
-
-def random_resize(img):
-    size = random.choice([320, 480, 512, 640])
-    return transforms.Resize((size, size))(img)
-
 
 transform = transforms.Compose([
             transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2.0)),
